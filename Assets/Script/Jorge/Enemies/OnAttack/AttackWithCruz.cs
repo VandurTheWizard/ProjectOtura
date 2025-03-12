731f134cf2393a6fba67dace3d2f847bc68bd1a7
dokozak <pokemonoscuroz@gmail.com>
2025-03-12 15:58:17 +0100

    Cambios

---
+++ Assets/Script/Jorge/Enemies/OnAttack/AttackWithCruz.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttackWithCruz : MonoBehaviour, Attack
+{
+    public float damagePerSecond = 1;
+    private LifeUsage life;
+    private bool isAttack = true;
+    private NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        life = GameObject.FindGameObjectWithTag("Player").GetComponent<LifeUsage>();
+    }
+    public void onAttack()
+    {
+        if (!isAttack)
+        {
+            return;
+        }
+        going();
+        life.loseLife(damagePerSecond * Time.deltaTime);
+    }
+
+    public void resetAttack()
+    {
+      
+    }
+
+    public void stopAttackFor(float seconds)
+    {
+        isAttack = false;
+        StartCoroutine(stopAttack(seconds));
+    }
+
+    private IEnumerator stopAttack(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        isAttack = true;
+
+    }
+
+    public bool going()
+    {
+        Vector3 destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 vision = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.LookAt(vision);
+        agent.SetDestination(destination);
+        return true;
+
+    }
+}
