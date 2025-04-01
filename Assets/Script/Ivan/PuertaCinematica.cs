dbf8cd16879507d5e118e5830ad63a82c9c2f7d6
VandurTheWizard <TheWizardVandur@gmail.com>
2025-04-01 11:56:03 +0200

    ttt

--- Assets/Script/Ivan/PuertaCinematica.cs
+++ Assets/Script/Ivan/PuertaCinematica.cs
@@ -5,7 +5,9 @@ public class PuertaCinematica : MonoBehaviour
     [Header("Configuración de las puertas")]
     public float rotatioDoor1;
     public float rotatioDoor2;
-    public float velocidad = 1;
+    public float velocidadMin = 0.1f;
+    public float velocidadMax = 1;
+    public float aceleracion = 0.2f;
     public GameObject door1;
     public GameObject door2;
 
@@ -14,19 +16,26 @@ public class PuertaCinematica : MonoBehaviour
     private float x2;
     private float y2;
 
+    private float velocidad;
+
 
 
     void Start()
     {
-        x1 = door1.transform.rotation.x;
-        y1 = door1.transform.rotation.y;
-        x2 = door2.transform.rotation.x;
-        y2 = door2.transform.rotation.y;
+        velocidad = velocidadMin;
+        x1 = door1.transform.eulerAngles.x;
+        y1 = door1.transform.eulerAngles.y;
+        x2 = door2.transform.eulerAngles.x;
+        y2 = door2.transform.eulerAngles.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (velocidad < velocidadMax)
+        {
+            velocidad += aceleracion * Time.deltaTime;
+        }
         Quaternion door1Rotation = Quaternion.Euler(x1, y1, rotatioDoor1);
         Quaternion door2Rotation = Quaternion.Euler(x2, y2, rotatioDoor2);
 
